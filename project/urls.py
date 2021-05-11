3a9c585d7a90775b16173f0b0ddef67b719a0bba
Julio <jn5.guape@gmail.com>
2021-05-11 11:42:18 -0300

    url pra editar e mudança no form.html

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import home, cadastrados, form
+from app.views import home, cadastrados, form, delete_person
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("cadastrados/", cadastrados, name="cadastrados"),
     path("form/", form, name="form"),
+    path("deletar/<int:id>/", delete_person, name="deletar"),
     path("", home, name="home"),
 ]
